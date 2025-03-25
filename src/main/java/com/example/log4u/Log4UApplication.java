428fed3d6e4219189696bcc1c64a0499739b9863
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-03-25 09:15:30 +0900

    init

---
+++ src/main/java/com/example/log4u/Log4UApplication.java
@@ -0,0 +1,13 @@
+package com.example.log4u;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Log4UApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Log4UApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/log4u/Log4UApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.log4u;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Log4UApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
