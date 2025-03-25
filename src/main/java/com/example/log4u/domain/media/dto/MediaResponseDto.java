e4c673ac208db89cb3835b08d00b06e66d30de13
TTaiJin <taeparis980110@gmail.com>
2025-03-25 15:14:15 +0900

    feat: Media 도메인 Mock Server 추가

---
+++ src/main/java/com/example/log4u/domain/media/dto/MediaResponseDto.java
@@ -0,0 +1,7 @@
+package com.example.log4u.domain.media.dto;
+
+public record MediaResponseDto(
+	Long mediaId,
+	String fileUrl
+) {
+}
---
+++ src/main/java/com/example/log4u/domain/media/service/MediaService.java
@@ -0,0 +1,12 @@
+package com.example.log4u.domain.media.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class MediaService {
+}
