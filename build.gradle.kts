7830aee14a2afe013e762517cdca051bb82f5461
ByeongGyu Jeon <cube9928@naver.com>
2025-03-25 10:45:39 +0900

    chore: jacoco, sonarcloud, checkstyle 옵션 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     java
     id("org.springframework.boot") version "3.4.4"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.sonarqube") version "6.0.1.5171"
+    jacoco
+    checkstyle
 }
 
 group = "com.example"
@@ -49,4 +52,27 @@ dependencies {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required = true
+        csv.required = false
+    }
+}
+
+checkstyle {
+    configFile = file("${rootDir}/naver-checkstyle-rules.xml")
+    configProperties["suppressionFile"] = "${rootDir}/naver-checkstyle-suppressions.xml"
+    toolVersion = "9.2"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "sapiens2000-dev_simple-sns")
+        property("sonar.organization", "sapiens2000-dev")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml")
+        property("sonar.java.checkstyle.reportPaths", "build/reports/checkstyle/main.xml")
+    }
 }
