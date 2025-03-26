5e68a39b3b6bb287b25e824a7b78aeae3a1d85f0
이건희 <dnzp74@g.eulji.ac.kr>
2025-03-26 17:31:47 +0900

    build: Security 관련 설정 임시 주석 처리 및 mysql 설정 추가
    - 구현 후 다시 변경 예정

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,13 +28,16 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
-    implementation("org.springframework.boot:spring-boot-starter-security")
+//    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+//    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("mysql:mysql-connector-java:8.0.33")
 
     compileOnly("org.projectlombok:lombok")
+    testCompileOnly("org.projectlombok:lombok")
+    testAnnotationProcessor("org.projectlombok:lombok")
     runtimeOnly("com.h2database:h2")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
