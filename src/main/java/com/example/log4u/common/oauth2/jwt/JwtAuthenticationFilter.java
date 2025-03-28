afd205504201b0d07dad45da92f4de897fddcd90
ByeongGyu Jeon <cube9928@naver.com>
2025-03-28 15:17:38 +0900

    feat: 쿠키에서 토큰 추출 로직

--- src/main/java/com/example/log4u/common/oauth2/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/example/log4u/common/oauth2/jwt/JwtAuthenticationFilter.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.example.log4u.common.oauth2.dto.CustomOAuth2User;
@@ -18,28 +19,49 @@ import com.example.log4u.domain.user.service.UserService;
 import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RequiredArgsConstructor
+@Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	private final JwtUtil jwtUtil;
-
 	private final UserService userService;
 
-	@Value("${jwt.access-token-expiration-time-seconds}")
+	@Value("${jwt.access-token-expire-time-seconds}")
 	private long accessTokenValiditySeconds;
 
-	@Value("${jwt.refresh-token-expiration-time-seconds}")
+	@Value("${jwt.refresh-token-expire-time-seconds}")
 	private long refreshTokenValiditySeconds;
 
 	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-		FilterChain filterChain) throws ServletException, IOException {
+	protected void doFilterInternal(
+		HttpServletRequest request,
+		@NonNull HttpServletResponse response,
+		@NonNull FilterChain filterChain
+	) throws ServletException, IOException {
+		String requestUri = request.getRequestURI();
+		if (requestUri.matches("^\\/login(?:\\/.*)?$")) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+		if (requestUri.matches("^\\/oauth2(?:\\/.*)?$")) {
+			filterChain.doFilter(request, response);
+			return;
+		}
 
-		// 헤더에서 access키에 담긴 토큰 추출
-		String accessToken = request.getHeader("access");
+		// 쿠키에서 access키에 담긴 토큰 추출
+		String accessToken = null;
+		Cookie[] cookies = request.getCookies();
+		for (Cookie cookie : cookies){
+			if (cookie.getName().equals("access")) {
+				accessToken = cookie.getValue();
+			}
+		}
 
 		// 토큰이 없다면 다음 필터로 넘겨서 발급 받아야함
 		if (accessToken == null) {
@@ -47,9 +69,16 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 			return;
 		}
 
+		log.info("필터에서 추출한 access: " + accessToken + "\n");
+
+
 		// 토큰 만료 확인, 만료 시 다음 필터로 넘기지 않음(재발급 필요)
 		try {
+			System.out.println("만료확인");
 			jwtUtil.isExpired(accessToken);
+			System.out.println("category : " + jwtUtil.getCategory(accessToken));
+			System.out.println("userId : " + jwtUtil.getUserId(accessToken));
+			System.out.println("role : " + jwtUtil.getRole(accessToken));
 		} catch (ExpiredJwtException e) {
 			PrintWriter writer = response.getWriter();
 			writer.print("토큰이 만료되었습니다.");
@@ -68,10 +97,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 			return;
 		}
 
+
 		// userId, role
 		Long userId = jwtUtil.getUserId(accessToken);
 
 		CustomOAuth2User customOAuth2User = new CustomOAuth2User(userService.getUserById(userId));
+		log.info("필터에서 추출한 userId: " + userId);
+		log.info("생성된 CustomOAuth2User ID: " + customOAuth2User.getUserId());
 
 		// security context holder 에 추가해줌
 		Authentication oAuth2Token = new UsernamePasswordAuthenticationToken(
