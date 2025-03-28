2be57449ea557ec4d7cb380a7b35efb9aad219b0
ByeongGyu Jeon <cube9928@naver.com>
2025-03-28 11:36:44 +0900

    feat: UserService 추가 UserRepository 수정

--- src/main/java/com/example/log4u/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/log4u/domain/user/repository/UserRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.log4u.domain.user.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-	Optional<User> findByUid(String username);
 	Optional<User> findByNickname(String nickname);
 	Optional<User> findByProviderId(String providerId);
 }
