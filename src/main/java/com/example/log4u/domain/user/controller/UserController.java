6ced77ee3295bcbc17f849c7c6736db47cbe4a35
ByeongGyu Jeon <cube9928@naver.com>
2025-03-28 11:39:20 +0900

    feat: 테스트용 UserController 추가

---
+++ src/main/java/com/example/log4u/domain/user/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.example.log4u.domain.user.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+	@GetMapping("")
+	public String modifyUserProfile(
+
+	){
+		return "test";
+	}
+}
