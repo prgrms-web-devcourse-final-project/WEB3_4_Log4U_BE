8768c8cee9be2d8ec40631052fb7b53b162cf39b
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-03-28 15:28:25 +0900

    refactor: Validation 용 어노테이션 추가

--- src/main/java/com/example/log4u/domain/reports/dto/ReportCreateRequestDto.java
+++ src/main/java/com/example/log4u/domain/reports/dto/ReportCreateRequestDto.java
@@ -2,9 +2,16 @@ package com.example.log4u.domain.reports.dto;
 
 import com.example.log4u.domain.reports.entity.Report;
 import com.example.log4u.domain.reports.reportType.ReportType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
 
 public record ReportCreateRequestDto(
+    @NotNull
     ReportType reportType,
+
+    @NotBlank
+    @Length(min = 2)
     String content
 ) {
     public Report toEntity(Report.ReportTargetType reportTargetType, Long targetId){
--- src/main/java/com/example/log4u/domain/supports/dto/SupportCreateRequestDto.java
+++ src/main/java/com/example/log4u/domain/supports/dto/SupportCreateRequestDto.java
@@ -3,9 +3,11 @@ package com.example.log4u.domain.supports.dto;
 import com.example.log4u.domain.supports.entity.Support;
 import com.example.log4u.domain.supports.supportType.SupportType;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Length;
 
 public record SupportCreateRequestDto(
+        @NotNull
         SupportType supportType,
 
         @NotBlank
