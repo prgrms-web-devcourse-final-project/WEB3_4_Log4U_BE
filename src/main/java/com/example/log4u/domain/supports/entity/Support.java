aeff9ee1cccba989022d93c57d832c2186c26d41
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-03-30 20:16:38 +0900

    refactor: develop rebase 이후 Support 엔티티 클래스에 column용 어노테이션 추가

--- src/main/java/com/example/log4u/domain/supports/entity/Support.java
+++ src/main/java/com/example/log4u/domain/supports/entity/Support.java
@@ -7,8 +7,11 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import com.example.log4u.domain.supports.supportType.SupportType;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -31,19 +34,26 @@ public class Support {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	private long requesterId;
+	@Column(nullable = false)
+	private Long requesterId;
 
+	@Column(nullable = false)
+	@Enumerated(value = EnumType.STRING)
 	private SupportType supportType;
 
+	@Column(nullable = false)
 	private String title;
 
+	@Column(nullable = false)
 	private String content;
 
 	@CreatedDate
 	private LocalDateTime createdAt;
 
 	@Setter
+	@Column(nullable = true)
 	private String answerContent;
 
+	@Column(nullable = true)
 	private LocalDateTime answeredAt;
 }
