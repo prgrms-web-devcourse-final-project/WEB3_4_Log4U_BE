195d3f5305e43ba78e9d92d2d1c8eb30921ca10d
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-03-31 09:36:42 +0900

    feat: MyPage 용 컨트롤러 클래스 추가

---
+++ src/main/java/com/example/log4u/domain/user/mypage/controller/MyPageController.java
@@ -0,0 +1,34 @@
+package com.example.log4u.domain.user.mypage.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.log4u.common.dto.PageResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class MyPageController {
+
+	@GetMapping("/users/me/likes")
+	public ResponseEntity<?> getMyLikesPage() {
+	}
+
+	@GetMapping("/users/me/followings")
+	public ResponseEntity<PageResponse<String>> getMyFollowingPage() {
+	}
+
+	@GetMapping("/users/me/followers")
+	public ResponseEntity<PageResponse<String>> getMyFollowerPage() {
+	}
+
+	@GetMapping("/users/me/diaries")
+	public ResponseEntity<PageResponse<String>> getMyDiaryPage() {
+	}
+
+	@GetMapping("/users/me/subscriptions")
+	public ResponseEntity<PageResponse<String>> getMySubscriptionsPage() {
+	}
+}
