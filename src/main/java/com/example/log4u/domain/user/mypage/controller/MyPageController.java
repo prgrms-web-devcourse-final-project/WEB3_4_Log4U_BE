b71c57b05592851a892380ce104a2534161f035a
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-03-31 11:33:29 +0900

    feat: 내 팔로우 / 팔로잉 조회 및 내가 작성한 다이어리 페이지 조회 컨트롤러 매핑 완료

--- src/main/java/com/example/log4u/domain/user/mypage/controller/MyPageController.java
+++ src/main/java/com/example/log4u/domain/user/mypage/controller/MyPageController.java
@@ -2,19 +2,27 @@ package com.example.log4u.domain.user.mypage.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.log4u.common.dto.PageResponse;
 import com.example.log4u.domain.diary.dto.DiaryResponseDto;
+import com.example.log4u.domain.user.dto.UserThumbnailResponseDto;
+import com.example.log4u.domain.user.mypage.service.MyPageService;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 public class MyPageController {
+	private final MyPageService myPageService;
 
 	@GetMapping("/users/me/diaries")
-	public ResponseEntity<PageResponse<DiaryResponseDto>> getMyDiaryPage() {
+	public ResponseEntity<PageResponse<DiaryResponseDto>> getMyDiaryPage(
+		@RequestParam(required = false) Long cursorId
+	) {
+		Long userId = 1L;
+		return ResponseEntity.ok(myPageService.getMyDiariesByCursor(userId, cursorId));
 	}
 
 	@GetMapping("/users/me/likes")
@@ -22,11 +30,19 @@ public class MyPageController {
 	}
 
 	@GetMapping("/users/me/followings")
-	public ResponseEntity<PageResponse<String>> getMyFollowingPage() {
+	public ResponseEntity<PageResponse<UserThumbnailResponseDto>> getMyFollowingPage(
+		@RequestParam(required = false) Long cursorId
+	) {
+		Long userId = 1L;
+		return ResponseEntity.ok(myPageService.getMyFollowings(userId, cursorId));
 	}
 
 	@GetMapping("/users/me/followers")
-	public ResponseEntity<PageResponse<String>> getMyFollowerPage() {
+	public ResponseEntity<PageResponse<UserThumbnailResponseDto>> getMyFollowerPage(
+		@RequestParam(required = false) Long cursorId
+	) {
+		Long userId = 1L;
+		return ResponseEntity.ok(myPageService.getMyFollowers(userId, cursorId));
 	}
 
 	@GetMapping("/users/me/subscriptions")
