6a4517fc413febbd421743ab334c1993d6b7d2d9
ByeongGyu Jeon <cube9928@naver.com>
2025-04-03 09:33:53 +0900

    fix: fixture 사용안하고 테스트

--- src/test/java/com/example/log4u/domain/follow/FollowTest.java
+++ src/test/java/com/example/log4u/domain/follow/FollowTest.java
@@ -2,16 +2,19 @@ package com.example.log4u.domain.follow;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
 
 import com.example.log4u.domain.follow.entitiy.Follow;
 import com.example.log4u.domain.follow.exception.FollowNotFoundException;
 import com.example.log4u.domain.follow.repository.FollowRepository;
 import com.example.log4u.domain.follow.service.FollowService;
+import com.example.log4u.domain.user.entity.SocialType;
 import com.example.log4u.domain.user.entity.User;
 import com.example.log4u.domain.user.exception.UserNotFoundException;
 import com.example.log4u.domain.user.repository.UserRepository;
@@ -21,7 +24,6 @@ import jakarta.transaction.Transactional;
 
 @DisplayName("팔로우 통합 테스트(시큐리티 제외)")
 @SpringBootTest
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class FollowTest {
 
 	@Autowired
@@ -33,14 +35,24 @@ class FollowTest {
 	@Autowired
 	private UserRepository userRepository;
 
-	private static final String WRONG_TARGET = "nonexistuser";
+	private static final String WRONG_TARGET = "nonExistUser";
 	private static final String TARGET = "targetUser";
 
 	@Test
 	@Transactional
 	@DisplayName("팔로우 시 유저가 없어 USER NOT FOUND 예외 발생")
 	void createFollowFailureWithUserNotFound() {
-		User user = UserFixture.createUserFixture();
+		User user = User.builder()
+			.name("test" + UUID.randomUUID())
+			.nickname("testUser" + UUID.randomUUID())
+			.providerId("123" + UUID.randomUUID())
+			.email("test" + UUID.randomUUID() + "@example.com")
+			.socialType(SocialType.KAKAO)
+			.role("ROLE_USER")
+			.statusMessage(LocalDateTime.now().toString())
+			.isPremium(false)
+			.build();
+
 		userRepository.save(user);
 
 		assertThrows(UserNotFoundException.class,
@@ -51,14 +63,32 @@ class FollowTest {
 	@Transactional
 	@DisplayName("팔로우가 되어야 한다.")
 	void createFollowSuccess() {
-		String targetUserNickname = "targetUser";
-		User initiator = UserFixture.createUserFixture();
-		User target = UserFixture.createUserFixtureWithNickname(targetUserNickname);
+		User initiator = User.builder()
+			.name("test" + UUID.randomUUID())
+			.nickname("testUser" + UUID.randomUUID())
+			.providerId("123" + UUID.randomUUID())
+			.email("test" + UUID.randomUUID() + "@example.com")
+			.socialType(SocialType.KAKAO)
+			.role("ROLE_USER")
+			.statusMessage(LocalDateTime.now().toString())
+			.isPremium(false)
+			.build();
+
+		User target = User.builder()
+			.name("test" + UUID.randomUUID())
+			.nickname(TARGET)
+			.providerId("123" + UUID.randomUUID())
+			.email("test" + UUID.randomUUID() + "@example.com")
+			.socialType(SocialType.KAKAO)
+			.role("ROLE_USER")
+			.statusMessage(LocalDateTime.now().toString())
+			.isPremium(false)
+			.build();
 
 		initiator = userRepository.save(initiator);
 		target = userRepository.save(target);
 
-		followService.createFollow(initiator.getUserId(), targetUserNickname);
+		followService.createFollow(initiator.getUserId(), TARGET);
 
 		assertTrue(followRepository.existsByInitiatorIdAndTargetId(initiator.getUserId(), target.getUserId()));
 	}
@@ -67,33 +97,81 @@ class FollowTest {
 	@Transactional
 	@DisplayName("팔로우 취소가 되어야한다.")
 	void deleteFollowSuccess() {
-		Long userId = saveOneFollow();
+		User initiator = User.builder()
+			.name("test" + UUID.randomUUID())
+			.nickname("testUser" + UUID.randomUUID())
+			.providerId("123" + UUID.randomUUID())
+			.email("test" + UUID.randomUUID() + "@example.com")
+			.socialType(SocialType.KAKAO)
+			.role("ROLE_USER")
+			.statusMessage(LocalDateTime.now().toString())
+			.isPremium(false)
+			.build();
 
-		followService.deleteFollow(userId, TARGET);
+		User target = User.builder()
+			.name("test" + UUID.randomUUID())
+			.nickname(TARGET)
+			.providerId("123" + UUID.randomUUID())
+			.email("test" + UUID.randomUUID() + "@example.com")
+			.socialType(SocialType.KAKAO)
+			.role("ROLE_USER")
+			.statusMessage(LocalDateTime.now().toString())
+			.isPremium(false)
+			.build();
+
+		final Long initiatorId = userRepository.save(initiator).getUserId();
+		target = userRepository.save(target);
+
+		Follow follow = Follow.of(
+			initiatorId,
+			target.getUserId()
+		);
+
+		followRepository.save(follow);
+		followService.deleteFollow(initiatorId, TARGET);
 
 		assertThrows(FollowNotFoundException.class,
-			() -> followService.deleteFollow(userId, TARGET));
+			() -> followService.deleteFollow(initiatorId, TARGET));
 	}
 
 	@Test
 	@Transactional
 	@DisplayName("팔로우한 정보가 없어 FollowNotFound 발생")
 	void deleteFollowFailureWithFollowNotFound() {
-		User user = UserFixture.createUserFixture();
-		user = userRepository.save(user);
-		final Long userId = user.getUserId();
+		User initiator = User.builder()
+			.name("test" + UUID.randomUUID())
+			.nickname("testUser" + UUID.randomUUID())
+			.providerId("123" + UUID.randomUUID())
+			.email("test" + UUID.randomUUID() + "@example.com")
+			.socialType(SocialType.KAKAO)
+			.role("ROLE_USER")
+			.statusMessage(LocalDateTime.now().toString())
+			.isPremium(false)
+			.build();
 
-		User target = UserFixture.createUserFixtureWithNickname(TARGET);
+		initiator = userRepository.save(initiator);
+		final Long initiatorId = initiator.getUserId();
+
+		User target = User.builder()
+			.name("test" + UUID.randomUUID())
+			.nickname(TARGET)
+			.providerId("123" + UUID.randomUUID())
+			.email("test" + UUID.randomUUID() + "@example.com")
+			.socialType(SocialType.KAKAO)
+			.role("ROLE_USER")
+			.statusMessage(LocalDateTime.now().toString())
+			.isPremium(false)
+			.build();
+
 		userRepository.save(target);
 
 		assertThrows(FollowNotFoundException.class,
-			() -> followService.deleteFollow(userId, TARGET));
+			() -> followService.deleteFollow(initiatorId, TARGET));
 	}
 
 	private Long saveOneFollow() {
-		String targetUserNickname = "targetUser";
 		User initiator = UserFixture.createUserFixture();
-		User target = UserFixture.createUserFixtureWithNickname(targetUserNickname);
+		User target = UserFixture.createUserFixtureWithNickname(TARGET);
 
 		initiator = userRepository.save(initiator);
 		target = userRepository.save(target);
