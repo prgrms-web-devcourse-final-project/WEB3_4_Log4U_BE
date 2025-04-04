13254056a670bea1c4bd3d070724c293d8b7af69
ByeongGyu Jeon <cube9928@naver.com>
2025-04-04 12:23:16 +0900

    feat: url 상수 추가

--- src/main/java/com/example/log4u/common/constants/UrlConstants.java
+++ src/main/java/com/example/log4u/common/constants/UrlConstants.java
@@ -1,7 +1,10 @@
 package com.example.log4u.common.constants;
 
 public class UrlConstants {
-	public static final String FRONT_URL = "http://localhost:3000";
+	public static final String FRONT_ORIGIN_URL = "http://localhost:3000";
+	public static final String PROFILE_MAKE_URL = FRONT_ORIGIN_URL + "/profile/make";
+	public static final String LOGIN_URL = FRONT_ORIGIN_URL + "/login";
+	public static final String MAIN_URL = FRONT_ORIGIN_URL + "/me";
 
 	// checkstyle 경고 제거
 	private UrlConstants() {
