88de81df8226da1ca74fef0085c140b9c7e56772
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-04-07 16:30:19 +0900

    refacotr: Auth에서 id 받아오도록 변경

--- src/main/java/com/example/log4u/domain/supports/controller/SupportController.java
+++ src/main/java/com/example/log4u/domain/supports/controller/SupportController.java
@@ -3,6 +3,7 @@ package com.example.log4u.domain.supports.controller;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.log4u.common.oauth2.dto.CustomOAuth2User;
 import com.example.log4u.domain.supports.dto.SupportCreateRequestDto;
 import com.example.log4u.domain.supports.dto.SupportGetResponseDto;
 import com.example.log4u.domain.supports.dto.SupportOverviewGetResponseDto;
@@ -28,19 +30,21 @@ public class SupportController {
 
 	@PostMapping
 	public ResponseEntity<Void> createSupport(
+		@AuthenticationPrincipal CustomOAuth2User customOAuth2User,
 		@RequestBody @Valid SupportCreateRequestDto supportCreateRequestDto
 	) {
-		long requesterId = 1L;
+		long requesterId = customOAuth2User.getUserId();
 		supportService.createSupport(requesterId, supportCreateRequestDto);
 		return new ResponseEntity<>(HttpStatus.CREATED);
 	}
 
 	@GetMapping
 	public ResponseEntity<Page<SupportOverviewGetResponseDto>> getSupportOverviewPage(
+		@AuthenticationPrincipal CustomOAuth2User customOAuth2User,
 		@RequestParam(defaultValue = "1") int page,
 		@RequestParam(required = false) SupportType supportType
 	) {
-		long requesterId = 1L;
+		long requesterId = customOAuth2User.getUserId();
 		Page<SupportOverviewGetResponseDto> supportOverviewPage = supportService.getSupportPage(requesterId, page,
 			supportType);
 		return ResponseEntity.ok().body(supportOverviewPage);
@@ -48,8 +52,9 @@ public class SupportController {
 
 	@GetMapping("/{supportId}")
 	public ResponseEntity<SupportGetResponseDto> getSupportBySupportId(
+		@AuthenticationPrincipal CustomOAuth2User customOAuth2User,
 		@PathVariable Long supportId) {
-		long requesterId = 1L;
+		long requesterId = customOAuth2User.getUserId();
 		SupportGetResponseDto supportGetResponseDto = supportService.getSupportById(requesterId, supportId);
 		return ResponseEntity.ok().body(supportGetResponseDto);
 	}
--- src/main/java/com/example/log4u/domain/user/mypage/controller/MyPageController.java
+++ src/main/java/com/example/log4u/domain/user/mypage/controller/MyPageController.java
@@ -1,11 +1,13 @@
 package com.example.log4u.domain.user.mypage.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.log4u.common.dto.PageResponse;
+import com.example.log4u.common.oauth2.dto.CustomOAuth2User;
 import com.example.log4u.domain.diary.VisibilityType;
 import com.example.log4u.domain.diary.dto.DiaryResponseDto;
 import com.example.log4u.domain.subscription.dto.SubscriptionResponseDto;
@@ -21,40 +23,46 @@ public class MyPageController {
 
 	@GetMapping("/users/me/diaries")
 	public ResponseEntity<PageResponse<DiaryResponseDto>> getMyDiaryPage(
+		@AuthenticationPrincipal CustomOAuth2User customOAuth2User,
 		@RequestParam(required = false, name = "visibilityType") VisibilityType visibilityType,
 		@RequestParam(required = false) Long cursorId
 	) {
-		Long userId = 1L;
+		long userId = customOAuth2User.getUserId();
 		return ResponseEntity.ok(myPageService.getMyDiariesByCursor(userId, visibilityType, cursorId));
 	}
 
 	@GetMapping("/users/me/likes")
 	public ResponseEntity<PageResponse<DiaryResponseDto>> getMyLikesPage(
+		@AuthenticationPrincipal CustomOAuth2User customOAuth2User,
 		@RequestParam(required = false) Long cursorId
 	) {
-		Long userId = 1L;
+		long userId = customOAuth2User.getUserId();
 		return ResponseEntity.ok(myPageService.getLikeDiariesByCursor(userId, cursorId));
 	}
 
 	@GetMapping("/users/me/followings")
 	public ResponseEntity<PageResponse<UserThumbnailResponseDto>> getMyFollowingPage(
+		@AuthenticationPrincipal CustomOAuth2User customOAuth2User,
 		@RequestParam(required = false) Long cursorId
 	) {
-		Long userId = 1L;
+		long userId = customOAuth2User.getUserId();
 		return ResponseEntity.ok(myPageService.getMyFollowings(userId, cursorId));
 	}
 
 	@GetMapping("/users/me/followers")
 	public ResponseEntity<PageResponse<UserThumbnailResponseDto>> getMyFollowerPage(
+		@AuthenticationPrincipal CustomOAuth2User customOAuth2User,
 		@RequestParam(required = false) Long cursorId
 	) {
-		Long userId = 1L;
+		long userId = customOAuth2User.getUserId();
 		return ResponseEntity.ok(myPageService.getMyFollowers(userId, cursorId));
 	}
 
 	@GetMapping("/users/me/subscriptions")
-	public ResponseEntity<SubscriptionResponseDto> getMySubscriptions() {
-		Long userId = 1L;
+	public ResponseEntity<SubscriptionResponseDto> getMySubscriptions(
+		@AuthenticationPrincipal CustomOAuth2User customOAuth2User
+	) {
+		long userId = customOAuth2User.getUserId();
 		return ResponseEntity.ok(myPageService.getMySubscription(userId));
 	}
 }
