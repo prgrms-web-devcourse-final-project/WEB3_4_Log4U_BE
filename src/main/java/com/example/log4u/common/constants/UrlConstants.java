ccec74a040080e9e8c4560caeb7cc1efd3b73194
ByeongGyu Jeon <cube9928@naver.com>
2025-04-09 12:24:43 +0900

    refactor: 리디렉션 위치 서브도메인으로 변경

--- src/main/java/com/example/log4u/common/constants/UrlConstants.java
+++ src/main/java/com/example/log4u/common/constants/UrlConstants.java
@@ -4,11 +4,11 @@ public class UrlConstants {
 
 	public static final String FRONT_ORIGIN_URL = "http://localhost:3000/me";
 	public static final String FRONT_SUB_DOMAIN_URL = "http://web.ec2-13-209-127-186.ap-northeast-2.compute.amazonaws.com:3000";
-	public static final String PROFILE_CREATE_URL = FRONT_ORIGIN_URL + "/profile/make";
-	public static final String LOGIN_URL = FRONT_ORIGIN_URL + "/login";
+	public static final String PROFILE_CREATE_URL = FRONT_SUB_DOMAIN_URL + "/profile/make";
+	public static final String LOGIN_URL = FRONT_SUB_DOMAIN_URL + "/login";
 
 	// 내 프로필 페이지가 메인
-	public static final String MAIN_URL = FRONT_ORIGIN_URL + "/me";
+	public static final String MAIN_URL = FRONT_SUB_DOMAIN_URL;
 
 	// checkstyle 경고 제거
 	private UrlConstants() {
