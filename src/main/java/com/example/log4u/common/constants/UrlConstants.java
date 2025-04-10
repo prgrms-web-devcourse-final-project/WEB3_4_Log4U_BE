23fa67cc789c2713e6ec50e8e7ef1d51d2aa30ea
ByeongGyu Jeon <cube9928@naver.com>
2025-04-10 16:41:24 +0900

    refactor: 프론트 도메인 수정

--- src/main/java/com/example/log4u/common/constants/UrlConstants.java
+++ src/main/java/com/example/log4u/common/constants/UrlConstants.java
@@ -2,15 +2,16 @@ package com.example.log4u.common.constants;
 
 public class UrlConstants {
 
-	public static final String FRONT_ORIGIN_URL = "http://localhost:3000/me";
+	public static final String FRONT_ORIGIN_URL = "http://web.ec2-13-209-127-186.ap-northeast-2.compute.amazonaws.com:3000/me";
 	public static final String FRONT_SUB_DOMAIN_URL = "http://web.ec2-13-209-127-186.ap-northeast-2.compute.amazonaws.com:3000";
 	public static final String PROFILE_CREATE_URL = FRONT_SUB_DOMAIN_URL + "/profile/make";
 	public static final String LOGIN_URL = FRONT_SUB_DOMAIN_URL + "/login";
 
 	// 내 프로필 페이지가 메인
-	public static final String MAIN_URL = FRONT_SUB_DOMAIN_URL;
+	public static final String MAIN_URL = FRONT_SUB_DOMAIN_URL + "/me";
 
 	// checkstyle 경고 제거
 	private UrlConstants() {
 	}
 }
+
