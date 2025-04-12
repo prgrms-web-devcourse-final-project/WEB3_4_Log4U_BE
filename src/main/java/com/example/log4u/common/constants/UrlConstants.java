d14d11d1e19237c2e90eb6d177e0e37307f919d8
ByeongGyu Jeon <cube9928@naver.com>
2025-04-12 23:06:37 +0900

    refactor: 리디렉션 url 변경

--- src/main/java/com/example/log4u/common/constants/UrlConstants.java
+++ src/main/java/com/example/log4u/common/constants/UrlConstants.java
@@ -5,6 +5,7 @@ public class UrlConstants {
 	public static final String FRONT_ORIGIN_URL = " https://web.log4u.site:3000";
 	public static final String FRONT_SUB_DOMAIN_URL = " https://web.log4u.site:3000";
 	public static final String FRONT_VERCEL_ORIGIN = "https://web-3-4-log4-u-fe.vercel.app";
+	
 	// 임시로 메인
 	public static final String PROFILE_CREATE_URL = FRONT_SUB_DOMAIN_URL + "";
 	public static final String LOGIN_URL = FRONT_SUB_DOMAIN_URL + "/login";
