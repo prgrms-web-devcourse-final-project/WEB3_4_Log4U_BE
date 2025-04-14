aee7945d60ed4f3b700ab21f89f1f8da77cacf08
ByeongGyu Jeon <cube9928@naver.com>
2025-04-14 14:26:04 +0900

    chore: url 변경

--- src/main/java/com/example/log4u/common/constants/UrlConstants.java
+++ src/main/java/com/example/log4u/common/constants/UrlConstants.java
@@ -4,14 +4,14 @@ public class UrlConstants {
 
 	public static final String FRONT_ORIGIN_URL = " https://web.log4u.site:3000";
 	public static final String FRONT_SUB_DOMAIN_URL = "https://web.log4u.site:3000";
-	public static final String FRONT_VERCEL_ORIGIN = "https://web-3-4-log4-u-fe.vercel.app";
+	public static final String FRONT_VERCEL_ORIGIN = "https://fe-log4u.vercel.app";
 
 	// 임시로 메인
-	public static final String PROFILE_CREATE_URL = FRONT_SUB_DOMAIN_URL + "";
-	public static final String LOGIN_URL = FRONT_SUB_DOMAIN_URL + "/login";
+	public static final String PROFILE_CREATE_URL = FRONT_VERCEL_ORIGIN + "";
+	public static final String LOGIN_URL = FRONT_VERCEL_ORIGIN + "/login";
 
 	// 내 프로필 페이지가 메인
-	public static final String MAIN_URL = FRONT_SUB_DOMAIN_URL + "/me";
+	public static final String MAIN_URL = FRONT_VERCEL_ORIGIN + "/me";
 
 	// checkstyle 경고 제거
 	private UrlConstants() {
