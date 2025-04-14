25e1e8b3cc9b0f557c340e6e400b8fa4ee9ee981
ByeongGyu Jeon <cube9928@naver.com>
2025-04-14 16:25:44 +0900

    refactor: cors 오류 테스트

--- src/main/java/com/example/log4u/common/constants/UrlConstants.java
+++ src/main/java/com/example/log4u/common/constants/UrlConstants.java
@@ -7,7 +7,7 @@ public class UrlConstants {
 	public static final String FRONT_VERCEL_ORIGIN = "https://fe-log4u.vercel.app";
 
 	// 임시로 메인
-	public static final String PROFILE_CREATE_URL = FRONT_VERCEL_ORIGIN + "";
+	public static final String PROFILE_CREATE_URL = FRONT_VERCEL_ORIGIN + "/";
 	public static final String LOGIN_URL = FRONT_VERCEL_ORIGIN + "login";
 
 	// 내 프로필 페이지가 메인
