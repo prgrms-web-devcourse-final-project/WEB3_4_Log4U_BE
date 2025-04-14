35757859d11ad3cb232fb57587d849a87ac61ffe
ByeongGyu Jeon <cube9928@naver.com>
2025-04-14 16:05:28 +0900

    refactor: 리디렉션 오류 테스트

--- src/main/java/com/example/log4u/common/constants/UrlConstants.java
+++ src/main/java/com/example/log4u/common/constants/UrlConstants.java
@@ -4,7 +4,7 @@ public class UrlConstants {
 
 	public static final String FRONT_ORIGIN_URL = " https://web.log4u.site:3000";
 	public static final String FRONT_SUB_DOMAIN_URL = "https://web.log4u.site:3000";
-	public static final String FRONT_VERCEL_ORIGIN = "https://fe-log4u.vercel.app/";
+	public static final String FRONT_VERCEL_ORIGIN = "https://fe-log4u.vercel.app";
 
 	// 임시로 메인
 	public static final String PROFILE_CREATE_URL = FRONT_VERCEL_ORIGIN + "";
