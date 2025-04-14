358cbff588ca001f9af9520ad11d4d79958f8498
ByeongGyu Jeon <cube9928@naver.com>
2025-04-15 03:05:14 +0900

    fix: 프로필 생성 로직 수정

--- src/main/java/com/example/log4u/common/constants/UrlConstants.java
+++ src/main/java/com/example/log4u/common/constants/UrlConstants.java
@@ -7,8 +7,8 @@ public class UrlConstants {
 	public static final String FRONT_VERCEL_ORIGIN = "https://fe-log4u.vercel.app";
 
 	// 임시로 메인
-	public static final String PROFILE_CREATE_URL = FRONT_VERCEL_ORIGIN + "/";
-	public static final String LOGIN_URL = FRONT_VERCEL_ORIGIN + "login";
+	public static final String PROFILE_CREATE_URL = FRONT_VERCEL_ORIGIN + "/profile/make";
+	public static final String LOGIN_URL = FRONT_VERCEL_ORIGIN + "/login";
 
 	// 내 프로필 페이지가 메인
 	public static final String MAIN_URL = FRONT_VERCEL_ORIGIN + "me";
