cc96326b9256d2555fc47e050995336336424e5a
ByeongGyu Jeon <cube9928@naver.com>
2025-04-14 14:34:51 +0900

    chore: cors 오류 수정

--- src/main/java/com/example/log4u/common/constants/UrlConstants.java
+++ src/main/java/com/example/log4u/common/constants/UrlConstants.java
@@ -4,14 +4,14 @@ public class UrlConstants {
 
 	public static final String FRONT_ORIGIN_URL = " https://web.log4u.site:3000";
 	public static final String FRONT_SUB_DOMAIN_URL = "https://web.log4u.site:3000";
-	public static final String FRONT_VERCEL_ORIGIN = "https://fe-log4u.vercel.app";
+	public static final String FRONT_VERCEL_ORIGIN = "https://fe-log4u.vercel.app/";
 
 	// 임시로 메인
 	public static final String PROFILE_CREATE_URL = FRONT_VERCEL_ORIGIN + "";
-	public static final String LOGIN_URL = FRONT_VERCEL_ORIGIN + "/login";
+	public static final String LOGIN_URL = FRONT_VERCEL_ORIGIN + "login";
 
 	// 내 프로필 페이지가 메인
-	public static final String MAIN_URL = FRONT_VERCEL_ORIGIN + "/me";
+	public static final String MAIN_URL = FRONT_VERCEL_ORIGIN + "me";
 
 	// checkstyle 경고 제거
 	private UrlConstants() {
