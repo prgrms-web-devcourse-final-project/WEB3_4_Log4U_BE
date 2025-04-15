85773a6e7ec5b4ee7be0881f1877782ed4307552
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-04-16 01:13:09 +0900

    feat: 특정 유저의 팔로잉/팔로워 목록 조회 API 추가

--- src/main/java/com/example/log4u/domain/user/mypage/service/MyPageService.java
+++ src/main/java/com/example/log4u/domain/user/mypage/service/MyPageService.java
@@ -3,8 +3,6 @@ package com.example.log4u.domain.user.mypage.service;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,12 +10,10 @@ import com.example.log4u.common.dto.PageResponse;
 import com.example.log4u.domain.diary.VisibilityType;
 import com.example.log4u.domain.diary.dto.DiaryResponseDto;
 import com.example.log4u.domain.diary.service.DiaryService;
-import com.example.log4u.domain.follow.repository.FollowQuerydsl;
 import com.example.log4u.domain.subscription.PaymentStatus;
 import com.example.log4u.domain.subscription.dto.SubscriptionResponseDto;
 import com.example.log4u.domain.subscription.entity.Subscription;
 import com.example.log4u.domain.subscription.repository.SubscriptionRepository;
-import com.example.log4u.domain.user.dto.UserThumbnailResponseDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -27,7 +23,6 @@ import lombok.RequiredArgsConstructor;
 public class MyPageService {
 	private final int defaultPageSize = 6;
 	private final DiaryService diaryService;
-	private final FollowQuerydsl followQuerydsl;
 	private final SubscriptionRepository subscriptionRepository;
 
 	@Transactional(readOnly = true)
@@ -41,32 +36,6 @@ public class MyPageService {
 	public PageResponse<DiaryResponseDto> getLikeDiariesByCursor(Long userId, Long cursorId) {
 		return diaryService.getLikeDiariesByCursor(userId, userId,
 			cursorId, defaultPageSize);
-	}
-
-	@Transactional(readOnly = true)
-	public PageResponse<UserThumbnailResponseDto> getMyFollowers(Long userId, Long cursorId, String keyword) {
-		Slice<UserThumbnailResponseDto> slice = followQuerydsl.getFollowerSliceByUserId(
-			userId,
-			cursorId,
-			keyword,
-			PageRequest.of(0, defaultPageSize));
-
-		Long nextCursor = !slice.isEmpty() ? slice.getContent().getLast().userId() : null;
-
-		return PageResponse.of(slice, nextCursor);
-	}
-
-	@Transactional(readOnly = true)
-	public PageResponse<UserThumbnailResponseDto> getMyFollowings(Long userId, Long cursorId, String keyword) {
-		Slice<UserThumbnailResponseDto> slice = followQuerydsl.getFollowingSliceByUserId(
-			userId,
-			cursorId,
-			keyword,
-			PageRequest.of(0, defaultPageSize));
-
-		Long nextCursor = !slice.isEmpty() ? slice.getContent().getLast().userId() : null;
-
-		return PageResponse.of(slice, nextCursor);
 	}
 
 	@Transactional(readOnly = true)
