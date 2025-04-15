63b81a8f765b54d90031590f81404f2650ac4619
이건희 <dnzp74@g.eulji.ac.kr>
2025-04-16 06:22:45 +0900

    refactor: 캐싱 적용후 성능테스트 진행하기 위한 클래스들 수정

--- performance-test.js
+++ performance-test.js
@@ -5,9 +5,10 @@ export const options = {
     stages: [
         { duration: '1m', target: 50 },   // 1분 동안 VU 50명 유지
         { duration: '1m', target: 100 },  // 1분 동안 VU 100명 유지
-        { duration: '1m', target: 200 },  // 1분 동안 VU 200명 유지
-        { duration: '1m', target: 300 },  // 1분 동안 VU 300명 유지
-        { duration: '1m', target: 500 },  // 1분 동안 VU 500명 유지
+        { duration: '1m', target: 300 },  // 1분 동안 VU 200명 유지
+        { duration: '1m', target: 500 },  // 1분 동안 VU 300명 유지
+        { duration: '1m', target: 700 },  // 1분 동안 VU 500명 유지
+        { duration: '1m', target: 1000 },  // 1분 동안 VU 500명 유지
         { duration: '1m', target: 0 },    // 점진적 종료
     ],
 };
@@ -17,20 +18,22 @@ const BASE_URL = 'http://spring:8080';
 export default function () {
     // 강북구 고정 좌표
     const bounds = {
-        south: 37.6335,
-        north: 37.6535,
-        west: 127.0011,
-        east: 127.0211,
+        south: 33.0,
+        north: 39.5,
+        west: 124.01,
+        east: 131.0,
+        zoom: 1
     };
 
-    const url = `${BASE_URL}/maps/diaries/marker?south=${bounds.south}&north=${bounds.north}&west=${bounds.west}&east=${bounds.east}`;
+    const url = `${BASE_URL}/maps/diaries/cluster?south=${bounds.south}&north=${bounds.north}&west=${bounds.west}&east=${bounds.east}&zoom=${bounds.zoom}`;
 
-    group('Get Diary Markers (Gangbuk-gu fixed bounds)', () => {
+    group('Get Diary Clusters (Gangbuk-gu fixed bounds)', () => {
         const res = http.get(url, {
             headers: {
                 'Content-Type': 'application/json',
             },
         });
+
 
         check(res, {
             'status is 200': (r) => r.status === 200,
