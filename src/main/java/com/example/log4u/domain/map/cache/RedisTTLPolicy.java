b85d2386d3c1162489ac045f51636512a624631c
이건희 <dnzp74@g.eulji.ac.kr>
2025-11-18 19:08:21 +0900

    refactor: Redis 캐시 로직 추상화한 코드로 수정

--- src/main/java/com/example/log4u/domain/map/cache/RedisTTLPolicy.java
+++ src/main/java/com/example/log4u/domain/map/cache/RedisTTLPolicy.java
@@ -3,7 +3,6 @@ package com.example.log4u.domain.map.cache;
 import java.time.Duration;
 
 public class RedisTTLPolicy {
-	public static final Duration DIARY_ID_SET_TTL = Duration.ofMinutes(60);
-	public static final Duration DIARY_TTL = Duration.ofMinutes(60);
+	public static final Duration MARKER_TTL = Duration.ofMinutes(60);
 	public static final Duration CLUSTER_TTL = Duration.ofMinutes(60);
 }
--- src/main/java/com/example/log4u/domain/map/cache/dao/ClusterCacheDao.java
+++ src/main/java/com/example/log4u/domain/map/cache/dao/ClusterCacheDao.java
@@ -1,56 +1,49 @@
 package com.example.log4u.domain.map.cache.dao;
 
-import java.time.Duration;
-import java.util.List;
-import java.util.Optional;
+import static com.example.log4u.common.config.redis.ObjectMapperFactory.*;
 
-import org.springframework.data.redis.core.RedisTemplate;
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
+import com.example.log4u.common.infra.cache.CacheManager;
 import com.example.log4u.domain.map.cache.CacheKeyGenerator;
 import com.example.log4u.domain.map.cache.RedisTTLPolicy;
 import com.example.log4u.domain.map.dto.response.DiaryClusterResponseDto;
 import com.example.log4u.domain.map.exception.InvalidMapLevelException;
-import com.example.log4u.domain.map.repository.sido.SidoAreasDiaryCountRepository;
 import com.example.log4u.domain.map.repository.sido.SidoAreasRepository;
-import com.example.log4u.domain.map.repository.sigg.SiggAreasDiaryCountRepository;
 import com.example.log4u.domain.map.repository.sigg.SiggAreasRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
-import com.fasterxml.jackson.core.type.TypeReference;
-
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class ClusterCacheDao {
 
-	private final RedisTemplate<String, String> redisTemplate;
-	private final ObjectMapper objectMapper;
+	private final CacheManager cacheManager;
+
 	private final SidoAreasRepository sidoAreasRepository;
 	private final SiggAreasRepository siggAreasRepository;
 
 	public List<DiaryClusterResponseDto> load(String geohash, int level) {
-		String key = CacheKeyGenerator.clusterCacheKey(geohash, level);
-		try {
-			String value = redisTemplate.opsForValue().get(key);
-			if (value == null){
-				return null;
-			}
-			return objectMapper.readValue(value, new TypeReference<>() {
-				});
-		} catch (Exception e) {
-			log.warn("[REDIS][CLUSTER][GET][ERR] key={}", key, e);
-			throw new RuntimeException(e);
+		String value = cacheManager.get(CacheKeyGenerator.clusterCacheKey(geohash, level));
+		if (value == null) {
+			return null;
 		}
+		return convertToClusters(value);
+	}
+
+	private List<DiaryClusterResponseDto> convertToClusters(String value) {
+		return readValue(value, new TypeReference<>() {
+		});
 	}
 
 	public List<DiaryClusterResponseDto> loadAndCache(String geohash, int level) {
 		List<DiaryClusterResponseDto> clusters = loadClustersFromDb(geohash, level);
-		cache(clusters,geohash, level);
+		cache(clusters, geohash, level);
 		return clusters;
 	}
 
@@ -64,13 +57,17 @@ public class ClusterCacheDao {
 
 	private void cache(List<DiaryClusterResponseDto> clusters, String geohash, int level) {
 		String key = CacheKeyGenerator.clusterCacheKey(geohash, level);
-
-		try {
-			String json = objectMapper.writeValueAsString(clusters);
-			redisTemplate.opsForValue().set(key, json, RedisTTLPolicy.CLUSTER_TTL);
-		} catch (Exception e) {
-			log.warn("클러스터 캐시 저장 실패 (geo={}, level={})", key, clusters.size(), e);
-			throw new RuntimeException(e);
-		}
+		cacheManager.cache(key, writeValueAsString(clusters), RedisTTLPolicy.CLUSTER_TTL);
 	}
+
+	public void evictSido(String geohash) {
+		String key = CacheKeyGenerator.clusterCacheKey(geohash, 1);
+		cacheManager.evict(key);
+	}
+
+	public void evictSigg(String geohash) {
+		String key = CacheKeyGenerator.clusterCacheKey(geohash, 2);
+		cacheManager.evict(key);
+	}
+
 }
