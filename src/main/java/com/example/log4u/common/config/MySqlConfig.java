9c078be290b4ba6c87d9fba197669dcd1e7eb305
이건희 <dnzp74@g.eulji.ac.kr>
2025-12-27 21:20:56 +0900

    refactor: test 환경에서 DataSource, Hikari 설정을 분리해 커넥션 풀 제어 가능하도록 개선

--- src/main/java/com/example/log4u/common/config/MySqlConfig.java
+++ src/main/java/com/example/log4u/common/config/MySqlConfig.java
@@ -5,21 +5,21 @@ import java.util.Map;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 
 @Configuration
 @EnableJpaRepositories(
@@ -41,36 +41,40 @@ import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 )
 public class MySqlConfig {
 
-	@Bean
-	@Primary
+	@Bean(name = "mysqlDataSourceProperties")
 	@ConfigurationProperties(prefix = "spring.datasource")
-	public HikariDataSource mysqlHikariDataSource() {
-		HikariDataSource dataSource = DataSourceBuilder.create()
-			.type(HikariDataSource.class)
-			.build();
+	public DataSourceProperties mysqlDataSourceProperties() {
+		return new DataSourceProperties();
+	}
 
-		dataSource.setMaximumPoolSize(10);
-		dataSource.setMinimumIdle(10);
-		dataSource.setIdleTimeout(30000);
-		dataSource.setConnectionTimeout(3000);
-		dataSource.setMaxLifetime(1800000);
-
-		return dataSource;
+	@Bean(name = "mysqlHikariConfig")
+	@ConfigurationProperties(prefix = "spring.datasource.hikari")
+	public HikariConfig mysqlHikariConfig() {
+		return new HikariConfig();
 	}
 
 	@Bean(name = "mysqlDataSource")
 	@Primary
 	public DataSource mysqlDataSource(
-		@Qualifier("mysqlHikariDataSource") HikariDataSource hikariDataSource
+		@Qualifier("mysqlDataSourceProperties") DataSourceProperties properties,
+		@Qualifier("mysqlHikariConfig") HikariConfig hikariConfig
 	) {
-		return new LazyConnectionDataSourceProxy(hikariDataSource);
+		HikariDataSource dataSource = properties.initializeDataSourceBuilder()
+				.type(HikariDataSource.class)
+				.build();
+
+		dataSource.setMaximumPoolSize(hikariConfig.getMaximumPoolSize());
+		dataSource.setMinimumIdle(hikariConfig.getMinimumIdle());
+		dataSource.setConnectionTimeout(hikariConfig.getConnectionTimeout());
+		dataSource.setIdleTimeout(hikariConfig.getIdleTimeout());
+		dataSource.setMaxLifetime(hikariConfig.getMaxLifetime());
+
+		return new LazyConnectionDataSourceProxy(dataSource);
 	}
 
 	@Bean(name = "mysqlEntityManagerFactory")
 	@Primary
-	public LocalContainerEntityManagerFactoryBean mysqlEntityManagerFactory(
-		@Qualifier("mysqlDataSource") DataSource dataSource
-	) {
+	public LocalContainerEntityManagerFactoryBean mysqlEntityManagerFactory(@Qualifier("mysqlDataSource") DataSource dataSource) {
 		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
 		em.setDataSource(dataSource);
 		em.setPackagesToScan(
@@ -84,8 +88,7 @@ public class MySqlConfig {
 			"com.example.log4u.domain.supports",
 			"com.example.log4u.domain.user",
 			"com.example.log4u.domain.subscription",
-			"com.example.log4u.domain.hashtag"
-		);
+			"com.example.log4u.domain.hashtag");
 
 		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
 		vendorAdapter.setShowSql(true);
@@ -93,7 +96,6 @@ public class MySqlConfig {
 		em.setJpaVendorAdapter(vendorAdapter);
 
 		Map<String, Object> properties = new HashMap<>();
-		properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
 		properties.put("hibernate.hbm2ddl.auto", "update");
 		properties.put("hibernate.format_sql", true);
 		em.setJpaPropertyMap(properties);
@@ -103,9 +105,7 @@ public class MySqlConfig {
 
 	@Bean(name = "mysqlTransactionManager")
 	@Primary
-	public PlatformTransactionManager mysqlTransactionManager(
-		@Qualifier("mysqlEntityManagerFactory") LocalContainerEntityManagerFactoryBean mysqlEmf
-	) {
+	public PlatformTransactionManager mysqlTransactionManager(@Qualifier("mysqlEntityManagerFactory") LocalContainerEntityManagerFactoryBean mysqlEmf) {
 		JpaTransactionManager transactionManager = new JpaTransactionManager();
 		transactionManager.setEntityManagerFactory(mysqlEmf.getObject());
 		return transactionManager;
--- src/main/java/com/example/log4u/common/config/PostgreSqlConfig.java
+++ src/main/java/com/example/log4u/common/config/PostgreSqlConfig.java
@@ -5,8 +5,8 @@ import java.util.Map;
 
 import javax.sql.DataSource;
 
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -14,39 +14,52 @@ import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 
 @Configuration
 @EnableJpaRepositories(
-	basePackages = {
-		"com.example.log4u.domain.map.repository"
-	},
+	basePackages = {"com.example.log4u.domain.map.repository"},
 	entityManagerFactoryRef = "postgresqlEntityManagerFactory",
 	transactionManagerRef = "postgresqlTransactionManager"
 )
 public class PostgreSqlConfig {
 
-	@Bean
+	@Bean(name = "postgresqlDataSourceProperties")
 	@ConfigurationProperties(prefix = "spring.second-datasource")
-	public HikariDataSource postgresqlHikariDataSource() {
-		return DataSourceBuilder.create()
-			.type(HikariDataSource.class)
-			.build();
+	public DataSourceProperties postgresqlDataSourceProperties() {
+		return new DataSourceProperties();
+	}
+
+	@Bean(name = "postgresqlHikariConfig")
+	@ConfigurationProperties(prefix = "spring.second-datasource.hikari")
+	public HikariConfig postgresqlHikariConfig() {
+		return new HikariConfig();
 	}
 
 	@Bean(name = "postgresqlDataSource")
 	public DataSource postgresqlDataSource(
-		@Qualifier("postgresqlHikariDataSource") HikariDataSource hikariDataSource
+		@Qualifier("postgresqlDataSourceProperties") DataSourceProperties properties,
+		@Qualifier("postgresqlHikariConfig") HikariConfig hikariConfig
 	) {
-		return new LazyConnectionDataSourceProxy(hikariDataSource);
+		HikariDataSource dataSource = properties.initializeDataSourceBuilder()
+				.type(HikariDataSource.class)
+				.build();
+
+		dataSource.setMaximumPoolSize(hikariConfig.getMaximumPoolSize());
+		dataSource.setMinimumIdle(hikariConfig.getMinimumIdle());
+		dataSource.setConnectionTimeout(hikariConfig.getConnectionTimeout());
+		dataSource.setIdleTimeout(hikariConfig.getIdleTimeout());
+		dataSource.setMaxLifetime(hikariConfig.getMaxLifetime());
+
+		return new LazyConnectionDataSourceProxy(dataSource);
 	}
 
 	@Bean(name = "postgresqlEntityManagerFactory")
-	public LocalContainerEntityManagerFactoryBean postgresqlEntityManagerFactory(
-		@Qualifier("postgresqlDataSource") DataSource dataSource
-	) {
+	public LocalContainerEntityManagerFactoryBean postgresqlEntityManagerFactory(@Qualifier("postgresqlDataSource") DataSource dataSource) {
 		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
 		em.setDataSource(dataSource);
 		em.setPackagesToScan("com.example.log4u.domain.map.entity");
@@ -57,7 +70,6 @@ public class PostgreSqlConfig {
 		em.setJpaVendorAdapter(vendorAdapter);
 
 		Map<String, Object> properties = new HashMap<>();
-		properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
 		properties.put("hibernate.hbm2ddl.auto", "update");
 		properties.put("hibernate.format_sql", true);
 		em.setJpaPropertyMap(properties);
@@ -66,9 +78,7 @@ public class PostgreSqlConfig {
 	}
 
 	@Bean(name = "postgresqlTransactionManager")
-	public PlatformTransactionManager postgresqlTransactionManager(
-		@Qualifier("postgresqlEntityManagerFactory") LocalContainerEntityManagerFactoryBean postgresqlEmf
-	) {
+	public PlatformTransactionManager postgresqlTransactionManager(@Qualifier("postgresqlEntityManagerFactory") LocalContainerEntityManagerFactoryBean postgresqlEmf) {
 		JpaTransactionManager transactionManager = new JpaTransactionManager();
 		transactionManager.setEntityManagerFactory(postgresqlEmf.getObject());
 		return transactionManager;
